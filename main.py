70b1f7fae17c4ef8c08010f61aaf20213bdddf99
VattiManojKumar <manojviru1@gmail.com>
2023-03-23 14:37:02 +0530

    templates

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask,redirect,url_for,render_template
+### Integrate HTML With Flask
+### HTTP verb GET And POST
+
+##Jinja2 template engine
+'''
+{%...%} conditions,for loops
+{{    }} expressions to print output
+{#....#} this is for comments
+'''
+from flask import Flask,redirect,url_for,render_template,request
 
 app=Flask(__name__)
-
 
 @app.route('/')
 def welcome():
@@ -9,22 +17,43 @@ def welcome():
 
 @app.route('/success/<int:score>')
 def success(score):
-    return " The score is verified and found  passed as te score is "+str(score)
+    res=""
+    if score>=50:
+        res="PASS"
+    else:
+        res='FAIL'
+    exp={'score':score,'res':res}
+    return render_template('result.html',result=exp)
 
-@app.route('/Fail/<int:score>')
-def Fail(score):
-    return " The score is verified and found failed as the score is  "+str(score)
 
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "The Person has failed and the marks is "+ str(score)
+
+### Result checker
 @app.route('/results/<int:marks>')
 def results(marks):
     result=""
-    if marks>50:
-       result="success"
+    if marks<50:
+        result='fail'
     else:
-        result="Fail"
-    
+        result='success'
     return redirect(url_for(result,score=marks))
 
+### Result checker submit html page
+@app.route('/submit',methods=['POST','GET'])
+def submit():
+    total_score=0
+    if request.method=='POST':
+        science=float(request.form['science'])
+        maths=float(request.form['maths'])
+        c=float(request.form['c'])
+        data_science=float(request.form['datascience'])
+        total_score=(science+maths+c+data_science)/4
+    res=""
+    return redirect(url_for('success',score=total_score))
+
+    
 
 
 
